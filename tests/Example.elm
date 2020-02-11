b90a43a669f591c8219c0b971edb2f7f71810bed
SimonLab <simon.labondance@gmail.com>
2020-02-11 16:18:44 +0100

    add .travis.yml file configurationm #3

---
+++ tests/Example.elm
@@ -0,0 +1,13 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    test "a simple test" <|
+    \_ ->
+        1
+        |> Expect.equal 1
